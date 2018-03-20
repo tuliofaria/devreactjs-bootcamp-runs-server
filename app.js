df1a96fd57663c0a0edadb6fdb45a01bcf78e452
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-03-20 19:52:08 -0300

    Adding Cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const bodyParser = require('body-parser')
+app.use(cors())
 app.use(bodyParser.json({ extended: true }))
 
 const routes = require('./routes')
